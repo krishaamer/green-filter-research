9b6809f8d780b8be110742d4f00728331bddaabb
Kris Haamer <kris@haam.co>
2022-10-05 14:12:02 +0300

    Improved loading

--- src/components/Embed.jsx
+++ src/components/Embed.jsx
@@ -5,12 +5,14 @@ export default function Embed() {
   return (
     <LoadingIframe
       skeleton={
-        <div className="default-embed-size">
-          <h2>Loading...</h2>
+        <div className="default-embed-size embed-b">
+          <div className="centered">
+            <h2>Loading...</h2>
+          </div>
         </div>
       }
       src="https://airtable.com/embed/shr4SJcuhnfPBKLXo?backgroundColor=red"
-      className="embed-b default-embed-size"
+      className="default-embed-size embed-b"
       frameBorder={0}
       width="100%"
       height="700px"
