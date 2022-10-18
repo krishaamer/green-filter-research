4018f434cfcc2c16030e35d3b4c1ea346a4c6114
Kris Haamer <kris@haam.co>
2022-10-18 22:34:05 +0300

    Add fragmented title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,13 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">Sustainability is</h1>
+        <div className="slashed-container">
+          <span className="slashed">
+            <span className="top" title="fragmented"></span>
+            <span className="bot" title="fragmented"></span>
+          </span>
+        </div>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
