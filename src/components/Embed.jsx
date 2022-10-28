375ee0af0de14eed3e0fc511376a71eef53f084c
Kris Haamer <kris@haam.co>
2022-10-28 20:30:41 +0300

    Revert "Switch to default iFrame"
    This reverts commit 8ee70c774ee67dbac86b42da08e6e7b1bc02c13f.

--- src/components/Embed.jsx
+++ src/components/Embed.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import Iframe from "react-iframe";
 import LoadingIframe from "react-loading-iframe";
 
 export default function Embed({ src, text = "Loading..." }) {
   return (
-    <Iframe
-      url={src}
-      loading="lazy"
-      id=""
-      display="block"
-      position="relative"
-      lassName="default-embed-size embed-b"
+    <LoadingIframe
+      skeleton={
+        <div className="default-embed-size default-skeleton embed-b">
+          <div className="centered">
+            <div id="spinner"></div>
+            <h2>{text}</h2>
+          </div>
+        </div>
+      }
+      src={src}
+      className="default-embed-size embed-b"
       frameBorder={0}
       width="100%"
       height="700px"
