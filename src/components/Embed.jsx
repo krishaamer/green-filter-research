b14466c2a5a18e6f14d3ffbd2cd247d35926174e
Kris Haamer <kris@haam.co>
2024-03-25 14:05:20 +0800

    Fix embeds

--- src/components/Embed.jsx
+++ src/components/Embed.jsx
@@ -2,26 +2,8 @@ import React from "react";
 
 export default function Embed({ src, text = "Loading..." }) {
   return (
-    <div className="relative w-96 h-96">
-      <iframe src={src} className="w-screen h-96"></iframe>
+    <div className="h700 w100p">
+      <iframe src={src} className="h700 w100p"></iframe>
     </div>
   );
 }
-
-
-/*
-<LoadingIframe
-  skeleton={
-    <div className="default-embed-size default-skeleton embed-b absolute relative">
-      <div className="centered absolute">
-        <div id="spinner"></div>
-        <h2>{text}</h2>
-      </div>
-    </div>
-  }
-  src={src}
-  className="default-embed-size embed-b"
-  width="100%"
-  height="700px"
-/>
-*/
